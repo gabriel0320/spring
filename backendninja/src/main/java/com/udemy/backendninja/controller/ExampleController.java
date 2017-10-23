aea96994ffc1a50175f10df678eb573b9f13e31e
Gabriel Orrego <gabriel0320@gmail.com>
2017-10-22 21:18:18 -0500

    Trabajando con las Vistas - Controladoras y Model

--- backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -1,10 +1,18 @@
 package com.udemy.backendninja.controller;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.udemy.model.Person;
+
+
 
 
 @Controller
@@ -15,15 +23,30 @@ public class ExampleController {
 	public static final String EXAMPLE_VIEW = "example";
 	
 	@GetMapping("/exampleString")
-	 public String exampleString() {
+	 public String exampleString(Model model) {
+		model.addAttribute("people", getPeople());
 		 return EXAMPLE_VIEW;
 		 
-	 }
+	 }	
 	// Segunda forma
 	
 	@GetMapping("/exampleMAV")
 	public ModelAndView exampleMAV() {
-		return new ModelAndView(EXAMPLE_VIEW);
+		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
+		mav.addObject("people",getPeople());
+		return mav;
+	}
+	
+	private List<Person> getPeople(){
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Jhon",23));
+		people.add(new Person("Gabriel",27));
+		people.add(new Person("Veronica",30));
+		people.add(new Person("Jacky",1));
+		people.add(new Person("Lennon",1));
+		
+		return people;
+		
 	}
 
 }
